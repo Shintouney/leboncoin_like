927f5814555bbbae82259238ce1b6145aed8e520
Soul Physician <shinta42@hotmail.fr>
2020-12-26 22:44:40 +0100

    finish with jquery

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,5 @@
 import "bootstrap"
-
+import "@fortawesome/fontawesome-free/js/all";
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
@@ -8,7 +8,15 @@ import "jquery-ui"
 window.jQuery = $;
 window.$ = $;
 import "bootstrap-submenu";
-$( document ).ready(function() {
-	$('[data-submenu]').submenupicker();
+$( document  ).on('turbolinks:load', function() {
+  $('[data-submenu]').submenupicker();
+})
+jQuery.noConflict()(function ($) { // this was missing for me
+  $(document).ready(function() {
+    $('[data-submenu]').submenupicker();
+  });
+});
+$(window).on('beforeunload', function(){
+  $('[data-submenu]').submenupicker();
 });
 console.log('Hello from application.js')
