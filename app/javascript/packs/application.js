326815c3e24c0594f6920f22bbf6a8014f2cd30c
Soul Physician <shinta42@hotmail.fr>
2020-12-28 22:46:59 +0100

    remove tab js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,18 +9,18 @@ window.jQuery = $;
 import "bootstrap-submenu";
 $( document  ).on('turbolinks:load', function() {
   $('[data-submenu]').submenupicker();
-	jQuery.noConflict()(function ($) {
-		$(document).ready(function() {
-			$('[data-submenu]').submenupicker();
-			const dropdown_elements = document.getElementById("dropdown-toggle");
-			dropdown_elements.addEventListener("click", function(event) {
-				console.log(event.target.innerText);
+  jQuery.noConflict()(function ($) {
+    $(document).ready(function() {
+      $('[data-submenu]').submenupicker();
+      const dropdown_elements = document.getElementById("dropdown-toggle");
+      dropdown_elements.addEventListener("click", function(event) {
+        console.log(event.target.innerText);
         const original_array = [];
-				const array = document.getElementById("main-field-dropdown").innerHTML.split("&nbsp;");
-				original_array.push(array[0], "&nbsp;", event.target.innerText);
-				document.getElementById("main-field-dropdown").innerHTML = original_array.join("");
+        const array = document.getElementById("main-field-dropdown").innerHTML.split("&nbsp;");
+        original_array.push(array[0], "&nbsp;", event.target.innerText);
+        document.getElementById("main-field-dropdown").innerHTML = original_array.join("");
         document.getElementById("search_category").value = event.target.innerText
-			});
-		});
-	});
+      });
+    });
+  });
 })
