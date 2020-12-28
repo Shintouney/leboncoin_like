0a349e1846dac2c33cfa863b5ea159dba903c82d
Soul Physician <shinta42@hotmail.fr>
2020-12-28 19:53:28 +0100

    finish js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,13 +6,20 @@ require("@rails/activestorage").start()
 require("channels")
 import "jquery-ui"
 window.jQuery = $;
-window.$ = $;
 import "bootstrap-submenu";
 $( document  ).on('turbolinks:load', function() {
   $('[data-submenu]').submenupicker();
+	jQuery.noConflict()(function ($) {
+		$(document).ready(function() {
+			$('[data-submenu]').submenupicker();
+			const dropdown_elements = document.getElementById("dropdown-toggle");
+			dropdown_elements.addEventListener("click", function(event) {
+				console.log(event.target.innerText);
+        const original_array = [];
+				const array = document.getElementById("main-field-dropdown").innerHTML.split("&nbsp;");
+				original_array.push(array[0], "&nbsp;", event.target.innerText);
+				document.getElementById("main-field-dropdown").innerHTML = original_array.join("");
+			});
+		});
+	});
 })
-jQuery.noConflict()(function ($) {
-  $(document).ready(function() {
-    $('[data-submenu]').submenupicker();
-  });
-});
