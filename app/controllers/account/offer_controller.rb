d2d9f4eaede970319376426255a8982a08fba5f5
Soul Physician <shinta42@hotmail.fr>
2020-12-29 22:06:12 +0100

    some change

--- app/controllers/account/offer_controller.rb
+++ app/controllers/account/offer_controller.rb
@@ -10,6 +10,7 @@ class Account::OfferController < AccountController
 
   def create
     if form_validate
+      @offer_form.validate_form
       @offer_form.save
       redirect_to root_path
     else
@@ -20,15 +21,16 @@ class Account::OfferController < AccountController
   private
 
   def offer_form
-    offer = Offer.new user_id: current_user&.id
+    offer = Offer.new \
+      user_id:  current_user&.id
     @offer_form ||= OfferForm.new(offer)
   end
 
   def offer_params
-    params.require(:offer).permit(:title, :price, :description)
+    params.require(:offer).permit(:title, :price, :description, :category_id)
   end
 
   def form_validate
-    @offer_form.validate(offer_params)
+    @offer_form.validate offer_params
   end
 end
--- app/forms/offer_form.rb
+++ app/forms/offer_form.rb
@@ -1,10 +1,18 @@
 class OfferForm < Reform::Form
 
   property :title, validates: { presence: true, allow_blank: false }
-  property :price, validates: { presence: true, allow_blank: false}
+  property :price, validates: { presence: true, allow_blank: false, numericality: { greater_than_or_equal_to: 0 }}
   property :description, validates: { presence: true, allow_blank: false }
   property :user_id, validates: { presence: true }
-  property :category_id, validates: { presence: true }
-  property :category
+  property :category_id, validates: { presence: true, allow_blank: false }
+
+  def validate_form
+    category = Category.find_by_name(self.category_id)
+    if category
+      self.category_id = category.id
+    else
+      errors.add(:category, "Catégorie invalide")
+    end
+  end
 
 end
