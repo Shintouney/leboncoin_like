7c92113d396eb59bb60a08770169e4796c390aef
Soul Physician <shinta42@hotmail.fr>
2021-01-04 00:48:23 +0100

    retab

--- spec/features/offer_creation_spec.rb
+++ spec/features/offer_creation_spec.rb
@@ -2,12 +2,12 @@ require 'rails_helper'
 
 feature "Offer creation", :devise do
   scenario "user create an offer", js: true do
-		visit(new_account_offer_path)
-		click_on "sign_in_link"
-		expect(current_path).to eq "/users/sign_in"
-		user     = FactoryBot.create(:user)
-		category = FactoryBot.create(:category)
-		only_sign_in(user.email, user.password)
+    visit(new_account_offer_path)
+    click_on "sign_in_link"
+    expect(current_path).to eq "/users/sign_in"
+    user     = FactoryBot.create(:user)
+    category = FactoryBot.create(:category)
+    only_sign_in(user.email, user.password)
     ap page.body
     click_on "Voiture"
     fill_in "offer_title", with: "Mon produit"
--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -2,23 +2,23 @@ require 'rails_helper'
 
 feature "Sign in", :devise do
   scenario "user cannot sign in if not registered" do
-		sign_in("nohiken42@gmail.com", "riveton")
-		expect(current_path).to eq "/users/sign_in"
+    sign_in("nohiken42@gmail.com", "riveton")
+    expect(current_path).to eq "/users/sign_in"
   end
 
-	scenario 'user can sign in with valid credentials' do
-		user = FactoryBot.create(:user)
-		sign_in(user.email, user.password)
-		expect(page).to have_content 'Connecté(e).'
-		expect(current_path).to eq "/"
-	end
+  scenario 'user can sign in with valid credentials' do
+    user = FactoryBot.create(:user)
+    sign_in(user.email, user.password)
+    expect(page).to have_content 'Connecté(e).'
+    expect(current_path).to eq "/"
+  end
 
-	scenario 'when user sign in from offers/new it get redirected to the form creation' do
-		visit(new_account_offer_path)
-		click_on "sign_in_link"
-		expect(current_path).to eq "/users/sign_in"
-		user = FactoryBot.create(:user)
-		only_sign_in(user.email, user.password)
-		expect(current_path).to eq "/account/offer/new"
-	end
+  scenario 'when user sign in from offers/new it get redirected to the form creation' do
+    visit(new_account_offer_path)
+    click_on "sign_in_link"
+    expect(current_path).to eq "/users/sign_in"
+    user = FactoryBot.create(:user)
+    only_sign_in(user.email, user.password)
+    expect(current_path).to eq "/account/offer/new"
+  end
 end
