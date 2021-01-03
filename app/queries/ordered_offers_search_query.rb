a15cc2c87a3d337a40e475128ad606b155cc68a6
Soul Physician <shinta42@hotmail.fr>
2021-01-03 21:25:32 +0100

    add queries ordered offer

--- app/queries/ordered_offers_search_query.rb
+++ app/queries/ordered_offers_search_query.rb
@@ -7,20 +7,45 @@ class OrderedOffersSearchQuery
   def call
     if @params[:category].blank? && @params[:text_search].blank? && @params[:location_search].blank?
       @relation.all.order(created_at: :desc)
-    elsif @params[:category].blank?
-      without_category_relation
     elsif @params[:text_search].blank? && @params[:location_search].blank?
       category_relation
+    elsif @params[:location_search].blank? && !@params[:category].blank?
+      without_location_relation
+    elsif @params[:category].blank? && @params[:location_search].blank?
+      only_text
+    elsif @params[:category].blank? && @params[:search_text].blank?
+      only_location
     else
       default_relation
     end
   end
 
-  private
-
   def category_relation
     category_id = Category.find_by_name(@params[:category])
     @relation.where(category_id: category_id)
+  end
+
+  def only_location
+    @relation.where(
+      location: @params[:location_search]
+    )
+  end
+
+  def only_text
+    @relation.where(
+      'title LIKE :search OR description LIKE :search',
+      search: "%#{@params[:text_search]}%"
+    )
+  end
+
+  def without_location_relation
+    category_id = Category.find_by_name(@params[:category])
+    @relation.where(
+      category_id: category_id
+    ).where(
+      'title LIKE :search OR description LIKE :search',
+      search: "%#{@params[:text_search]}%"
+    )
   end
 
   def default_relation
@@ -35,12 +60,4 @@ class OrderedOffersSearchQuery
     )
   end
 
-  def without_category_relation
-    @relation.where(
-      'title LIKE :search OR description LIKE :search',
-      search: "%#{@params[:text_search]}%"
-    ).where(
-      location: @params[:location_search]
-    )
-  end
 end
