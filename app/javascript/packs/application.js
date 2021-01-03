7e28099c3aa0c26794774d0042206483e6ff5f6b
Soul Physician <shinta42@hotmail.fr>
2021-01-03 16:11:42 +0100

    add js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,6 +12,17 @@ $( document  ).on('turbolinks:load', function() {
   jQuery.noConflict()(function ($) {
     $(document).ready(function() {
       $('[data-submenu]').submenupicker();
+      if (window.location.href == "http://localhost:3000/") {
+        const search_data = document.getElementById("search_data");
+        search_data.addEventListener("input", function(event) {
+          const text_search            = document.getElementById("text_search");
+          const text_search_origin     = document.getElementById("text_search_origin");
+          const location_search        = document.getElementById("location_search");
+          const location_search_origin = document.getElementById("location_search_origin");
+          text_search.value     = text_search_origin.value
+          location_search.value = location_search_origin.value
+        });
+      }
       const dropdown_elements = document.getElementById("dropdown-toggle");
       dropdown_elements.addEventListener("click", function(event) {
         const original_array = [];
